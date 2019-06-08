046006c702921192c1370e3ab88579de554f9da4
Jesse <sajia1986@126.com>
2019-06-08 23:43:34 +0800

    wip

---
+++ ssh/ssh.py
@@ -0,0 +1,17 @@
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect(hostname='192.168.137.101', port=22, username='hugo', password='1234567890')
+stdin, stdout, stderr = ssh.exec_command('ls -lrt')
+
+result = stdout.read()
+
+print(result.decode())
+ssh.close()
+
+# pip3 freeze > req.text
+# pip3 download -d .\packages -r req.text
+
+# pip install --no-index --find-index=.\packages -r req.txt
